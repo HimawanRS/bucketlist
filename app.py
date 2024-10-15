abeffd911c139fc26290f0451113b347e10a0055
HimawanRS <hmwnrzk@gmail.com>
2024-10-15 14:35:40 +0700

    freeze dotenv and other requirements

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 
-client = MongoClient('mongodb+srv://admin:admin@cluster0.3tltw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
-db = client.dbsparta
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME =  os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+
+db = client[DB_NAME]
 
 app = Flask(__name__)
 
@@ -35,7 +46,7 @@ def bucket_done():
 @app.route("/delete", methods=["POST"])
 def delete_bucket():
     num_receive = request.form["num_give"]
-    db.bucket.delete_one({'num': int(num_receive)})  # Hapus item dari MongoDB berdasarkan 'num'
+    db.bucket.delete_one({'num': int(num_receive)})
     return jsonify({'msg': 'Item successfully deleted!'})
 
 @app.route("/bucket", methods=["GET"])
